88001bcd792d2d1712ff1992352be0867d291c83
cindymikoda <mikodacindy@gmail.com>
2025-10-30 04:57:17 +0800

    Moved all the svg to /src/assets and imported to socialData.js

--- src/library/socialData.js
+++ src/library/socialData.js
@@ -1,26 +1,31 @@
+import instagramIcon from "../assets/instagram-icon.svg";
+import linkedinIcon from "../assets/linkedin-icon.svg";
+import githubIcon from "../assets/github-icon.svg";
+import emailIcon from "../assets/email-icon.svg";
+
 export const socialLinks = [
   {
     id: "instagram",
     label: "Instagram",
     href: "https://instagram.com/mikodacindy",
-    icon: "public/instagram-icon.svg",
+    icon: instagramIcon,
   },
   {
     id: "linkedin",
     label: "LinkedIn",
     href: "https://linkedin.com/in/cindy-mikoda-9951b8194/",
-    icon: "public/linkedin-icon.svg",
+    icon: linkedinIcon,
   },
   {
     id: "github",
     label: "GitHub",
     href: "https://github.com/cindymikoda",
-    icon: "public/github-icon.svg",
+    icon: githubIcon,
   },
   {
     id: "email",
     label: "Email",
     href: "mailto:cmikodak@gmail.com",
-    icon: "public/email-icon.svg",
+    icon: emailIcon,
   },
 ];
