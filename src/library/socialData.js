4e0adfa5522b65bad280feaffb317d6cc36fb50d
cindymikoda <mikodacindy@gmail.com>
2025-10-30 04:54:19 +0800

    Icon Path for vercel

--- src/library/socialData.js
+++ src/library/socialData.js
@@ -1,31 +1,26 @@
-import instagramIcon from "/public/instagram-icon.svg";
-import linkedinIcon from "/public/linkedin-icon.svg";
-import githubIcon from "/public/github-icon.svg";
-import emailIcon from "/public/email-icon.svg";
-
 export const socialLinks = [
   {
     id: "instagram",
     label: "Instagram",
     href: "https://instagram.com/mikodacindy",
-    icon: instagramIcon,
+    icon: "public/instagram-icon.svg",
   },
   {
     id: "linkedin",
     label: "LinkedIn",
     href: "https://linkedin.com/in/cindy-mikoda-9951b8194/",
-    icon: linkedinIcon,
+    icon: "public/linkedin-icon.svg",
   },
   {
     id: "github",
     label: "GitHub",
     href: "https://github.com/cindymikoda",
-    icon: githubIcon,
+    icon: "public/github-icon.svg",
   },
   {
     id: "email",
     label: "Email",
     href: "mailto:cmikodak@gmail.com",
-    icon: emailIcon,
+    icon: "public/email-icon.svg",
   },
 ];
