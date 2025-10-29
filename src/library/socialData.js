513e558bb3e6f05fa80f133415dee4c2be7f6c3f
cindymikoda <mikodacindy@gmail.com>
2025-10-30 04:45:29 +0800

    Still trying to fix icon paths

--- src/library/socialData.js
+++ src/library/socialData.js
@@ -1,26 +1,31 @@
+import instagramIcon from "/public/instagram-icon.svg";
+import linkedinIcon from "/public/linkedin-icon.svg";
+import githubIcon from "/public/github-icon.svg";
+import emailIcon from "/public/email-icon.svg";
+
 export const socialLinks = [
   {
     id: "instagram",
     label: "Instagram",
     href: "https://instagram.com/mikodacindy",
-    icon: "./instagram-icon.svg",
+    icon: instagramIcon,
   },
   {
     id: "linkedin",
     label: "LinkedIn",
     href: "https://linkedin.com/in/cindy-mikoda-9951b8194/",
-    icon: "./linkedin-icon.svg",
+    icon: linkedinIcon,
   },
   {
     id: "github",
     label: "GitHub",
     href: "https://github.com/cindymikoda",
-    icon: "./github-icon.svg",
+    icon: githubIcon,
   },
   {
     id: "email",
     label: "Email",
     href: "mailto:cmikodak@gmail.com",
-    icon: "./email-icon.svg",
+    icon: emailIcon,
   },
 ];
