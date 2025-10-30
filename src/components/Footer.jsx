10505dd1d6f0d13949662b1a8278a0fde1e2c457
cindymikoda <mikodacindy@gmail.com>
2025-10-30 20:21:02 +0800

    Testing new colors for the background. The svg icons were moved to assets folder to work smoothly on Vercel, but do not work on local host.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,7 @@
 export default function Footer({ isDark, toggleTheme }) {
   return (
     <div className="card-section footer-section">
-      <p className="footer-text">
-        Made with love by Cindy |
-        <a
-          href="https://github.com/cindymikoda/Mikoda"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Repository
-        </a>
-      </p>
-
+      <p className="footer-text">Made with love by Cindy</p>
       <div className="theme-toggle-wrapper">
         <label className="toggle-switch">
           <input
